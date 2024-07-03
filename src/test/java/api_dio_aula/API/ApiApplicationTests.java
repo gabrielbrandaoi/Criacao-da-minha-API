e9301f3e1d2984ab583cb77aeeac632fad64d7a6
Gabriel Brandão <106843179+gabrielbrandaoi@users.noreply.github.com>
2024-07-02 21:42:22 -0300

    Add files via upload

---
+++ src/test/java/api_dio_aula/API/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package api_dio_aula.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
